7a2fb5264dc019ccfdb98fd042405cbe1ac025aa
TBruyn <timtbruyn@gmail.com>
2018-11-15 18:17:26 +0100

    Add MessageTest and update TimestampTest to JUnit 5

--- src/test/java/TimestampTest.java
+++ src/test/java/TimestampTest.java
@@ -1,10 +1,9 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 
 public class TimestampTest {
 
