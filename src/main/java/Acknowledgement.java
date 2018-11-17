ccaa93db54864ab9905e74057a55c16db62e0cc6
TBruyn <timtbruyn@gmail.com>
2018-11-17 19:15:54 +0100

    Implement Acknowledgement and add test class

--- src/main/java/Acknowledgement.java
+++ src/main/java/Acknowledgement.java
@@ -1,8 +1,28 @@
+import java.util.Objects;
+
 public class Acknowledgement {
     private int messageSender;
     private Timestamp timestamp;
 
-    public Acknowledgement() {
+    public Acknowledgement(int messageSender, Timestamp timestamp) {
+        this.messageSender = messageSender;
+        this.timestamp = timestamp;
+    }
 
+    public int getMessageSender() {
+        return messageSender;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acknowledgement acknowledgement = (Acknowledgement) o;
+        return messageSender == acknowledgement.getMessageSender() &&
+                Objects.equals(timestamp, acknowledgement.getTimestamp());
     }
 }
