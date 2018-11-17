f24e3201b677d43eb8352e8fbf5f0cc51b6f72e6
TBruyn <timtbruyn@gmail.com>
2018-11-17 19:18:29 +0100

    Implement AcknowledgementTest

--- src/test/java/AcknowledgementTest.java
+++ src/test/java/AcknowledgementTest.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AcknowledgementTest {
+
+    @Test
+    void testGet() {
+        Timestamp stamp = new Timestamp(new int[]{1, 2, 3});
+        int id = 3;
+        Acknowledgement message = new Acknowledgement(id, stamp);
+        assertEquals(id, message.getMessageSender());
+        assertEquals(stamp, message.getTimestamp());
+    }
+
+    @Test
+    void testEquals() {
+        Acknowledgement first = new Acknowledgement(1, new Timestamp(new int[]{1, 2, 3}));
+        Acknowledgement second = new Acknowledgement(1, new Timestamp(new int[]{1, 2, 3}));
+        assertEquals(first, second);
+    }
 }
