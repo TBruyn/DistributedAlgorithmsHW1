9aa01b99774ee330ecc84bfa71b35d03026318a0
TBruyn <timtbruyn@gmail.com>
2018-12-17 17:12:17 +0100

    Ordinary

--- src/main/java/ex3/Ordinary.java
+++ src/main/java/ex3/Ordinary.java
@@ -7,8 +7,26 @@ public class Ordinary {
     private int potentialFather;
     private int father;
 
-    public void receive(Message msg) {
+    public Ordinary() {
+        level = ownerId = potentialFather = father = -1;
+    }
 
+    public void receive(Message msg) {
+        switch (msg.compareTo(level, ownerId)) {
+            case '<':
+                break;
+            case '>':
+                potentialFather = msg.getSender();
+                level = msg.getLevel();
+                ownerId = msg.getId();
+                if (father == -1) father = potentialFather;
+                RMIUtil.candidateSend(msg, father);
+                break;
+            case '=':
+                father = potentialFather;
+                RMIUtil.candidateSend(msg, father);
+                break;
+        }
 
     }
 
