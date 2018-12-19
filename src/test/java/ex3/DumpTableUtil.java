d46666ddf4a7f170701a0a2a2cc9d68a83fbc2d3
Jannes Timm <jayco@gmx.de>
2018-12-19 17:21:18 +0100

    add dump data method

---
+++ src/test/java/ex3/DumpTableUtil.java
@@ -0,0 +1,11 @@
+package ex3;
+
+import java.util.List;
+import java.util.Map;
+
+public class DumpTableUtil {
+
+    public static void dumpData(List<Map<String, Integer>> msgData, Map<Integer, Integer> maxLevels) {
+        
+    }
+}
